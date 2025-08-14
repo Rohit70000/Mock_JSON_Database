826b779cce531eaafaff470dafc7bd875c44ecf0
Rohit70000 <rohitgangwani2005@gmail.com>
2025-08-14 11:20:08 -0600

    comment

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express= require("express");
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
+const ejs= require('ejs');
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
 
-app.listen(PORT,()=>{
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+mongoose.connect(uri).then(
+async ()=>{
+
+console.log(`Connected to Mongoose Server`);
+        app.listen(PORT,()=>{
     console.log(`Connected to port ${PORT}`);
 });
+}).catch((err)=>console.log('error:', (err)));
